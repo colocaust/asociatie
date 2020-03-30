980b8988002d060b8e668cc900b25d47f8ef8abf
Emilian <emilian@192-168-0-134.rdsnet.ro>
2020-03-30 12:31:44 +0300

    - Bara navigare- Angular Materialize CSS + JS- Angular jQuery- Componenta Home ( Acasa )- Initializare bara navigare Materilize ( App Component )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as M from 'materialize-css';
 
+declare var $: any;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls:
+		[
+			'./app.component.scss'
+		]
 })
-export class AppComponent {
-  title = 'asociatie';
+export class AppComponent implements OnInit {
+	title = 'asociatie';
+
+	ngOnInit(): void {
+		document.addEventListener('DOMContentLoaded', function() {
+			var elems = document.querySelectorAll('.sidenav');
+			var instances = M.Sidenav.init(elems, {});
+		});
+	}
 }
